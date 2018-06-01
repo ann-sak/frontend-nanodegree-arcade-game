38108198df90271abb20cba8b7b7e93977d08ceb
Anna Sakowska <ann@iMac-Anna.local>
2018-06-01 21:16:12 +0200

    Changes to ES6

--- js/app.js
+++ js/app.js
@@ -1,25 +1,27 @@
 
-var Enemy = function(x,y) {
+class Enemy{
+  constructor(x,y) {
     this.x = x;
     this.y = y;
     this.sprite = 'images/enemy-bug.png';
-    this.speed = 15 * Math.random();
+    this.speed = Math.floor(Math.random() * 5) + 10;
     this.width = 50;
     this.height = 80;
 }
 
 //Update the enemy
-Enemy.prototype.update = function(dt) {
+  update (dt) {
     this.x = this.x + Math.random() * this.speed;
     if (this.x > 401){
        this.x = 0;
    }
-};
+ };
 
 // Draw the enemy on the screen
-Enemy.prototype.render = function() {
+  render () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  }
+}
 
 // Enemies our player must avoid
 var enemy1 = new Enemy(0,80);
@@ -28,60 +30,60 @@ var enemy3 = new Enemy(0,160);
 
 const allEnemies = [enemy1, enemy2, enemy3];
 
-var Player = function(x,y) {
-  this.sprite = 'images/char-horn-girl.png';
-  this.x = x;
-  this.y = y;
-  this.width = 50;
-  this.height = 80;
-}
+class Player {
+  constructor (x,y) {
+    this.sprite = 'images/char-horn-girl.png';
+    this.x = x;
+    this.y = y;
+    this.width = 50;
+    this.height = 80;
+  }
 
 //Update the player
-Player.prototype.update = function(dt){
-  this.dt = dt;
-  checkCollisions();
-  win();
-};
+  update (dt){
+    this.dt = dt;
+    checkCollisions();
+    win();
+  };
 
 //Render the player
-Player.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+  handleInput(direction){
+    if (direction === 'up') {
+      if (this.y === 400) {
+        this.y = 400;
+      } else {
+        this.y += 80;
+      }
+    }
+
+    if (direction === 'down') {
+      if (this.y === 0) {
+        this.y = 0;
+      } else {
+        this.y -= 80;
+      }
+    }
+
+    if (direction === 'right') {
+      if (this.x === 400) {
+        this.x = 400;
+      } else {
+        this.x += 100;
+      }
+    }
+
+    if (direction === 'left') {
+      if (this.x === 0) {
+        this.x = 0;
+      } else {
+        this.x -= 100;
+      }
+    }
+  }
 }
-
-
-Player.prototype.handleInput = function(direction){
-  if (direction === 'up') {
-    if (this.y === 400) {
-      this.y = 400;
-    } else {
-      this.y += 80;
-    }
-  }
-
-  if (direction === 'down') {
-    if (this.y === 0) {
-      this.y = 0;
-    } else {
-      this.y -= 80;
-    }
-  }
-
-  if (direction === 'right') {
-    if (this.x === 400) {
-      this.x = 400;
-    } else {
-      this.x += 100;
-    }
-  }
-
-  if (direction === 'left') {
-    if (this.x === 0) {
-      this.x = 0;
-    } else {
-      this.x -= 100;
-    }
-  }
-};
 
 var player = new Player(200,400);
 
