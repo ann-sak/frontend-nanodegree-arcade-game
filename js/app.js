3e34b1272c7c48902ff38d962d58ff1b6e526126
Anna Sakowska <ann@iMac-Anna.local>
2018-06-11 11:30:37 +0200

    Add win function and modal at the end of game

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ class Enemy{
     this.speed = speed;
     this.width = 50;
     this.height = 80;
-}
+}//end enemy constructor
 
 //Update the enemy
   update (dt) {
@@ -17,23 +17,23 @@ class Enemy{
     }
 
     if (this.y == player.y && (this.x > player.x - 70 && this.x < player.x + 60)) {
-        player.collision = true;
+        player.collision = true; //check collision
     };
- };
+ };//end enemies update
 
 // Draw the enemy on the screen
   render () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
-}
+}//end Enemy class
 
-// Enemies our player must avoid
-var enemy1 = new Enemy(0,80, 0);
-var enemy2 = new Enemy(0,240,0);
-var enemy3 = new Enemy(0,160,0);
-/*var enemy1 = new Enemy(0,80, Math.random() * 5 );
-var enemy2 = new Enemy(0,240, Math.random() * 5 + 5);
-var enemy3 = new Enemy(0,160, Math.random() * 5 + 10);*/
+//generate the enemies
+const enemy1 = new Enemy(0,80, Math.random() * 5 );
+const enemy2 = new Enemy(0,240, Math.random() * 5 + 5);
+const enemy3 = new Enemy(0,160, Math.random() * 5 + 10);
+/*const enemy4 = new Enemy(0,80, Math.random() * 5 );
+const enemy5 = new Enemy(0,240, Math.random() * 4 + 10);
+const enemy6 = new Enemy(0,160, Math.random() * 6 + 10);*/
 const allEnemies = [enemy1, enemy2, enemy3];
 
 class Player {
@@ -46,75 +46,73 @@ class Player {
     this.unmoveable = false;
     this.win = false;
     this.collision = false;
-  }
+  }//end players constructor
 
+//the players move directions
   handleInput(direction){
+      if (direction === 'down') {//the player goes down
+        if (this.y === 400) {
+          this.y = 400;
+        } else {
+          this.y += 80;
+        }
+      }
 
-            if (direction === 'down') {
-              if (this.y === 400) {
-                this.y = 400;
-
-              } else {
-                this.y += 80;
-              }
+      if (direction === 'up') {//the player goes up
+        if (this.y > 0){
+          this.y -= 80;
+            if (this.y === 0) {
+            this.y = 0;
+            setTimeout(() => {
+              this.win = true;
+              setTimeout(() => {this.win = false}, 20);
+            }, 100);
             }
+        }
+      }
 
-            if (direction === 'up') {
+      if (direction === 'right') {//the player goes right
+        if (this.x === 400) {
+          this.x = 400;
+        } else {
+          this.x += 100;
+        }
+      }
 
-              if (this.y > 0){
-                this.y -= 80;
-                console.log(this.y);
-
-                  if (this.y === 0) {
-                  this.y = 0;
-                  setTimeout(() => {
-                    this.win = true;
-                    setTimeout(() => {this.win = false}, 10);
-                  }, 100);
-                  console.log(this.y)
-                  }
-              }
-            }
-
-
-
-
-            if (direction === 'right') {
-              if (this.x === 400) {
-                this.x = 400;
-              } else {
-                this.x += 100;
-              }
-            }
-
-            if (direction === 'left') {
-              if (this.x === 0) {
-                this.x = 0;
-              } else {
-                this.x -= 100;
-              }
-            }
-
-  }
+      if (direction === 'left') {//the player goes left
+        if (this.x === 0) {
+          this.x = 0;
+        } else {
+          this.x -= 100;
+        }
+      }
+    }//end handleInput
 
 //Update the player
   update (dt){
     this.dt = dt;
 
-    if (this.win) {
+    if (this.win) {//win function
       score.count += 500;
       lives.reset();
       this.reset();
     }
 
-    if (this.collision) {
+    if (this.collision) {//check collision
       this.reset();
       lives.count--;
       score.count -=50;
-      setTimeout(() => { this.collision = false; }, 10);
-    }
+      setTimeout(() => {this.collision = false;}, 10);
 
-  }
+
+    }
+    const modal = document.getElementById('modal');
+    if (lives.count === 0) {
+      modal.style.display = "block";
+      this.x = x;
+      this.y = y;
+    }
+  }//end players update
 
 //Render the player
   render() {
@@ -127,18 +125,15 @@ class Player {
   }
 
 
-};
+}; //end Player class
+const player = new Player(200,400);//generate player
 
-var player = new Player(200,400);
-
-class Lives {
+class Lives {//hearts
     constructor() {
         this.x = 10;
         this.y = 0;
-
         this.count = 5;
         this.sprite = 'images/Heart.png';
-
         const spriteWidth = 40;
         const spriteHeight = 60;
     }
@@ -168,10 +163,10 @@ class Lives {
             xPos += this.x + 30;
         }
     }
-}
-var lives = new Lives();
+}//end lives class
+const lives = new Lives();//generate lives
 
-class Score {
+class Score {//scores panel
     constructor() {
         this.x = 20;
         this.y = 600;
@@ -189,9 +184,11 @@ class Score {
         ctx.font = "15px Arial";
         ctx.fillText(this.count, this.x + 105, this.y);
     }
-}
+}//end score class
+const score = new Score();//generate score
 
-var score = new Score();
+
+
 
 // This listens for key presses and sends the keys to
 // Player.handleInput() method.
